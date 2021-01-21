03447970bd426b71e0ee471fc7e4dddf00502bd2
enes abdüllatif yakacı <aenes1996@hotmail.com>
2021-01-22 01:08:13 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<String, int> telefonKodu = new Dictionary<string, int>();
+            telefonKodu.Add("Ankara", 312);
+            telefonKodu.Add("Antalya", 242);
+            telefonKodu.Add("Eskişehir", 222);
+            telefonKodu.Add("Isparta", 246);
+            telefonKodu.Add("İstanbul", 212);
+            telefonKodu.Add("Kütahya", 274);
+
+            foreach (var telefon in telefonKodu)
+            {
+                Console.WriteLine(telefon);
+            }
+        }
+    }
+}
